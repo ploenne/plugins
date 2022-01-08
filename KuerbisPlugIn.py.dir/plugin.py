c1c7daf5418e33a1981271d273e62b4bca5155cc
Emma <emma@ploenne.net>
2022-01-08 19:46:10 +0100

    KÜRBISE!!!
    Kürbistreppe

--- KuerbisPlugIn.py.dir/plugin.py
+++ KuerbisPlugIn.py.dir/plugin.py
@@ -7,59 +7,22 @@ class kuerbisplugin(PythonPlugin):
         pass
     
     def onCommand(self, sender, command, label, args):
+        originalPosition = sender.getLocation()
+        # self.getServer().broadcastMessage( "Hallo " + sender.name + " deine Position ist: " + str(position))
         
-        # wie es im Buch gesagt wird 
-        # eingabe  = args[0]
-        # 
-        # zahl = int(eingabe)
-        # 
-        # zahl = zahl + 1
-        # 
-        # info = "Ergebnis " + str(zahl)
-        # 
-        # self.getServer().broadcastMessage("Hallo " + str(sender.name) + " dein Ergbnis ist " + info)
-        # 
-        # return True
-    
-        if len(args) == 0:
-            self.getServer().broadcastMessage( "Hallo " + sender.name + " du hast keinen Wert angegeben! Setze eine Zahl ein!" ) 
-            return True
+        #position.setX(position.getX() + 3)
         
-        if len(args) == 1: 
-        
-            wert = args[0]
-        
-            self.getLogger().info("uebergebener wert: " + wert)
+        for x in range(1,10):
             
-            if wert.isdigit():
-                
-                    zahl = int(wert)
-                
-                    zahl2 = zahl + 1
-                
-                    self.getServer().broadcastMessage( "Hallo " + sender.name + " dein Ergebnis ist " + str(zahl2)
-                                                      ) 
-            else:
-                    self.getServer().broadcastMessage("Du darfst nur Zahlen einstetzen, keine Woerter!")
-                
-            return True
-        
-        else:
-            
-            summe = 0
-            
-            for item in args:
-                    summe = summe + int(item)
-            
-            self.getServer().broadcastMessage( "Hallo " + sender.name + " deine Rechenaufgabe ergibt " + str(summe) ) 
-
-            return True  
-            
-            
-                
-            
-                
-                
-                
-        
-        
+            kuerbisPosition = sender.getLocation()
+            #self.getLogger().info("vor: " + str(x) + ":  " + str(kuerbisPosition))
+            kuerbisPosition.setX(kuerbisPosition.getX() + float(x) + 1 ) 
+            kuerbisPosition.setY(kuerbisPosition.getY() + float(x) - 1)
+           # self.getLogger().info("nach: " + str(x) + ":  " + str(kuerbisPosition))
+            welt = sender.getWorld()
+            block = welt.getBlockAt(kuerbisPosition)
+            block.setType(bukkit.Material.PUMPKIN)
+                    
+        return True 
+     
+     
