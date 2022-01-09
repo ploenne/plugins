1837e09570b130bb22dc0f9d34f38bb415e2831f
Emma <emma@ploenne.net>
2022-01-09 19:53:25 +0100

    Breite Kürbistreppe

--- KuerbisPlugIn.py.dir/plugin.py
+++ KuerbisPlugIn.py.dir/plugin.py
@@ -3,7 +3,7 @@
 
 class kuerbisplugin(PythonPlugin):
     def onEnable(self):
-        self.getLogger().info("--- Start Plus Eins PlugIn Enable")
+        self.getLogger().info(" Start Kuerbis ")
         pass
     
     def onCommand(self, sender, command, label, args):
@@ -12,16 +12,55 @@ class kuerbisplugin(PythonPlugin):
         
         #position.setX(position.getX() + 3)
         
-        for x in range(1,10):
+        if len(args) > 0: 
+        
+            wert = args[0]
             
-            kuerbisPosition = sender.getLocation()
-            #self.getLogger().info("vor: " + str(x) + ":  " + str(kuerbisPosition))
-            kuerbisPosition.setX(kuerbisPosition.getX() + float(x) + 1 ) 
-            kuerbisPosition.setY(kuerbisPosition.getY() + float(x) - 1)
-           # self.getLogger().info("nach: " + str(x) + ":  " + str(kuerbisPosition))
-            welt = sender.getWorld()
-            block = welt.getBlockAt(kuerbisPosition)
-            block.setType(bukkit.Material.PUMPKIN)
+            if wert == "clear":
+                
+                groesse = 100
+                
+                haelfte = groesse / 2
+                    
+                for x in range(0,groesse):
+                
+                    for y in range(0,groesse):
+                        
+                        # self.getLogger().info("x: " + str(x) + "y:  " + str(y))
+                        
+                        for z in range(0,groesse):
+                            
+                            kuerbisPosition = sender.getLocation()
+                            
+                            kuerbisPosition.setX(kuerbisPosition.getX() + float(x - haelfte)) 
+                            kuerbisPosition.setY(kuerbisPosition.getY() + float(y - haelfte))
+                            kuerbisPosition.setZ(kuerbisPosition.getZ() + float(z - haelfte)) 
+                           # self.getLogger().info("nach: " + str(x) + ":  " + str(kuerbisPosition))
+                            welt = sender.getWorld()
+                            block = welt.getBlockAt(kuerbisPosition)
+                            
+                            if block.getType() == bukkit.Material.PUMKIN:
+                                block.setType(bukkit.Material.AIR)
+                
+                
+                return True
+        else:
+                
+            for z in range(1,4):
+                
+                for x in range(1,11):
+                        
+                    kuerbisPosition = sender.getLocation()
+                    #self.getLogger().info("vor: " + str(x) + ":  " + str(kuerbisPosition))
+                    kuerbisPosition.setX(kuerbisPosition.getX() + float(x) + 1 ) 
+                    kuerbisPosition.setY(kuerbisPosition.getY() + float(x) - 1 )
+                    kuerbisPosition.setZ(kuerbisPosition.getZ() + float(z) - 1 ) 
+                   # self.getLogger().info("nach: " + str(x) + ":  " + str(kuerbisPosition))
+                    welt = sender.getWorld()
+                    block = welt.getBlockAt(kuerbisPosition)
+                    block.setType(bukkit.Material.PUMKIN)
+                    #block.setType(bukkit.Material.COBBLESTONE_STAIRS) ??
+                    #block.setFacingDirection(BlockFace.EAST , True) ??
                     
         return True 
      
